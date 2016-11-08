d457e6d7b9091fcce70384a3e00d452e35e9c977
Dan Miller <dsmiller95@gmail.com>
2016-11-07 19:43:29 -0600

    added neural network demo implementation for FANN

--- FANNBirdAnalysis/FANNBirdAnalysis/NeuralNetAttempt.cs
+++ FANNBirdAnalysis/FANNBirdAnalysis/NeuralNetAttempt.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace FANNBirdAnalysis
 {
-    public class Class1
+    public class NeuralNetAttempt
     {
+        
     }
 }
---
+++ FANNBirdAnalysis/FANNUnitTests/NeuralNetTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FANNUnitTests
+{
+    [TestClass]
+    public class NeuralNetTest
+    {
+        [TestMethod]
+        public void NetworkTest()
+        {
+
+        }
+    }
+}
