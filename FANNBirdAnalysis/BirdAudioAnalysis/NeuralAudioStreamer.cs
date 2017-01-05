bee275fcfd74610527654b972d71aca371ba125e
Rachel Duchesneau <rachelduch95@gmail.com>
2017-01-04 18:38:11 -0600

    Added todos for new comments that are needed

--- FANNBirdAnalysis/BirdAudioAnalysis/NeuralAudioStreamer.cs
+++ FANNBirdAnalysis/BirdAudioAnalysis/NeuralAudioStreamer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// TODO: like before, maybe explain what these data types are and how they are chosen
 #if FANN_FIXED
 using FANNCSharp.Fixed;
 using DataType = System.Int32;
@@ -19,7 +20,7 @@ namespace BirdAudioAnalysis
 {
 
     /**
-     * Class built to stream an audio file through a neural network; after it has been trained ideally
+     * Class built to stream an audio file through a neural network. Ideally, the network should have been trained first
      */
     class NeuralAudioStreamer
     {
@@ -27,7 +28,7 @@ namespace BirdAudioAnalysis
         private readonly AudioAnalyzer _analyzer;
         private string _fileName;
         private readonly NeuralNet _network;
-
+        // TODO: why are these numbers the ones you chose and if someone is changing the neural network, which of these numbers should be changed?
         public NeuralAudioStreamer(NeuralNet network, string fileName, int bufferSize = 4096, int sampleRate = 44100)
         {
             _fileName = fileName;
@@ -40,6 +41,7 @@ namespace BirdAudioAnalysis
          */
         public IEnumerable<DataType[]> GetResultStream()
         {
+            // TODO: why are you getting the input length and data size in this context and what are you doing with it
             var inputLength = _network.InputCount;
             int frequencySize = _analyzer.GetDataSize();
             if(inputLength % frequencySize != 0)
@@ -51,6 +53,7 @@ namespace BirdAudioAnalysis
             //An array to hold all the data for the input layer of the neural network
             var rollingWindow = new DataType[inputLength];
             var frequencies = _analyzer.GetFrequencies();
+            // TODO: for this whole thing you explain each line but not really why youre doing it. so include that.
             foreach (var current in frequencies)
             {
                 DataType[] transferBuffer = new DataType[inputLength];
@@ -62,7 +65,7 @@ namespace BirdAudioAnalysis
 
                 rollingWindow = transferBuffer;
                 
-                //yeild the output result of the neural network
+                //yield the output result of the neural network
                 yield return _network.Run(rollingWindow);
             }
         }
