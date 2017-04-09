be5035a95fe12296a7f1828e9e91a985d8ced065
Dan Miller <dsmiller95@gmail.com>
2017-04-09 14:36:29 -0500

    added some more shell classes representing some of the work we'll probably have to do

---
+++ BirdCallExtractor/BirdCallSplitter/AudioFFTSplitter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BirdAudioAnalysis
+{
+    public class AudioFFTSplitter
+    {
+        public AudioFFTSplitter()
+        {
+            
+        }
+
+        /// <summary>
+        /// Accepts a stream of FFT results, and returns a new IEnumerable composed of streams of FFT data
+        ///     which have been deemed acceptable by the trimmer
+        /// </summary>
+        /// <param name="FFTData">A stream of FFT data directly from the file</param>
+        /// <returns>a new IEnumerable composed of streams of FFT data which have been deemed acceptable by the trimmer</returns>
+        public IEnumerable<IEnumerable<float[]>> splitAudioStream(IEnumerable<float[]> FFTData)
+        {
+            return null;
+        }
+    }
+}
---
+++ BirdCallExtractor/BirdCallSplitter/AudioSaver.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirdAudioAnalysis
+{
+    public class AudioSaver
+    {
+        public AudioSaver()
+        {
+            
+        }
+
+        /// <summary>
+        /// Save a stream of FFT data to a file. Will perform the reverse FFT and save it to a file
+        /// </summary>
+        /// <param name="fileName">The file to save the data to</param>
+        /// <param name="data">the FFT data</param>
+        /// <returns>String pointing to the file which was saved</returns>
+        public async Task<string> saveAsAudioFile(string fileName, IEnumerable<float[]> data)
+        {
+            return null;
+        }
+    }
+}
