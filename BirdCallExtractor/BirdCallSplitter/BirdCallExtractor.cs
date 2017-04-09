9f847c69da2b9a689412a94e87f2990747f848cc
Dan Miller <dsmiller95@gmail.com>
2017-04-09 14:14:31 -0500

    added main function

--- BirdCallExtractor/BirdCallSplitter/BirdCallExtractor.cs
+++ BirdCallExtractor/BirdCallSplitter/BirdCallExtractor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,11 +13,28 @@ namespace BirdAudioAnalysis
 		private static void Main(string[] args)
 		{
 			Console.WriteLine("Press S to split up the audio");
+
+
             
+		    string splittingRoot = "..\\..\\..\\DataSets\\AudioToSplit\\";
+
             var key = Console.ReadKey();
-		    if (key.KeyChar == 'S' || key.KeyChar == 's')
-		    {
-		        
+		    if (key.KeyChar == 'S' || key.KeyChar == 's'){
+		        var splitter = new AudioFileSplitAndTrim();
+		        var resultFiles = splitter.ProcessTheseFiles(new string[] {
+                        splittingRoot + "01.mp3",
+                        splittingRoot + "02.mp3",
+                        splittingRoot + "03.mp3",
+                        splittingRoot + "04.mp3",
+                        splittingRoot + "05.mp3"
+                    }, splittingRoot + "\\Split");
+                resultFiles.GetAwaiter().OnCompleted(() =>
+                                                     {
+                                                         foreach (var file in resultFiles.Result)
+                                                         {
+                                                             Console.Out.WriteLine(file);
+                                                         }
+                                                     });
 		    }
 
             Console.WriteLine("Press the any key to exit");
